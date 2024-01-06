66a035a1f1e3c36f2c104bcf0da77e8b25bd15b5
amirakhaled2027 <155392660+amirakhaled2027@users.noreply.github.com>
2024-01-06 02:13:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+//to reverse the order of the array
+let arr = ['amira', 'khaled', 'ahmed', 'saad'];
+console.log(arr.reverse());
+
+//to make the array in an alphabetical order
+console.log(arr.sort());
+
+let numbers = [6, 2, 8, 1, 0, 7];
+console.log(numbers.sort());
